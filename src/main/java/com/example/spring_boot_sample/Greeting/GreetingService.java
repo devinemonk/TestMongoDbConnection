38c5f1a7326f3760da4f204a8d73b2d79212748a
Kushagra Gupta <kushagragupta@INKACTXM305.local>
2024-09-23 19:12:06 +0530

    sample

---
+++ src/main/java/com/example/spring_boot_sample/Greeting/GreetingService.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_sample.Greeting;
+
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingService {
+
+    public String getGreetingMessage() {
+        return "Hello, Welcome to Spring Boot!";
+    }
+}
--- src/main/java/com/example/spring_boot_sample/SpringBootSampleApplication.java
+++ src/main/java/com/example/spring_boot_sample/SpringBootSampleApplication.java
@@ -2,12 +2,13 @@ package com.example.spring_boot_sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.example.spring_boot_sample", "Greeting"})
 public class SpringBootSampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSampleApplication.class, args);
 	}
-
 }
